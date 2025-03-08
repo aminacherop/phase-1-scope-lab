b5ad2b509342d0a3f0a7efc1a3ace8f2a598dd5a
aminacherop <cheropamina26@gmail.com>
2025-03-08 17:09:17 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+var customerName = `bob`
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase();
+
+}
+console.log(customerName());
+console.log(upperCaseCustomerName());
+
+
+var bestCustomer 
+function setBestCustomer (){
+    bestCustomer = `not bob`
+
+}
+setBestCustomer();
+console.log(bestCustomer());
+
+var bestCustomer = `bob`
+function overwriteBestCustomer(){
+    bestCustomer = `maybe bob`
+
+}
+overwriteBestCustomer();
+console.log(bestCustomer());
+
+
+const leastFavoriteCustomer = "Amina"; 
+
+function changeLeastFavoriteCustomer() {
+    try {
+        leastFavoriteCustomer = "Alice"; 
+        return leastFavoriteCustomer;
+    } catch (error) {
+        throw new Error("Assignment to constant variable.");
+    }
+}
+
+console.log(changeLeastFavoriteCustomer());
+
+
